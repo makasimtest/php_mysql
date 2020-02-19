118cd42980f3451c7e89bc5fddf7c751aa4522a5
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2020-02-19 12:55:57 +0200

    upd

--- usercode.php
+++ usercode.php
@@ -5,33 +5,14 @@ use Aws\S3\Exception\S3Exception;
 
 require_once __DIR__.'/vendor/autoload.php';
 
-$mysqli = null;
-
 function handle($data) {
-    global $mysqli;
+    $pdo = new PDO($data['mysql_dsn'], $data['mysql_user'], $data['mysql_pass']);
 
-    if ($mysqli === null) {
-        $mysqli = new mysqli($data['host'], $data['user'], $data['pass'], $data['db']);
+    $stm = $pdo->prepare("SELECT * FROM users WHERE id= ?");
+    $stm->bindValue(1, $data['user_id']);
+    $stm->execute();
 
-        if ($mysqli->connect_errno) {
-            throw new \Exception(sprintf(
-                "An error occurred when trying to establish a connection to the database: %s %d",
-                $mysqli->error,
-                $mysqli->errno
-            ));
-        }
-    }
-
-    if (false == $mysqli->ping()) {
-        $mysqli = null;
-        throw new \Exception("Connection lost");
-    }
-
-    $select_query = "SELECT id, name, email FROM users WHERE id=".$mysqli->real_escape_string($data['id']);
-    $result = $mysqli->query($select_query);
-    if ($user = mysqli_fetch_array($result)) {
-        $data['user'] = $user;
-    }
+    $data['user'] = $stm->fetch(PDO::FETCH_ASSOC);
 
     return $data;
 }
